49ec0f511e07187d9e497fedca84d20052147cda
EricLiu27 <eric.liu82794@gmail.com>
2023-09-07 08:54:52 -0400

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Dojo_Survey_With_Model.Models;
+
+namespace Dojo_Survey_With_Model.Controllers;
+
+public class HomeController : Controller
+{
+    static SurveyModel surveymodel;
+
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost("submit")]
+    public IActionResult Submit(SurveyModel newSurveyModel)
+    {
+        surveymodel = newSurveyModel;
+        return RedirectToAction("Display");
+    }
+
+    [HttpGet("display")]
+    public IActionResult Display()
+    {
+        return View(surveymodel);
+    }
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/SurveyModel.cs
@@ -0,0 +1,14 @@
+#pragma warning disable CS8618
+namespace Dojo_Survey_With_Model.Models;
+
+public class SurveyModel
+{
+
+    public string Name { get; set; }
+
+    public string Location { get; set; }
+
+    public string Language { get; set; }
+
+    public string Comment { get; set; }
+}
